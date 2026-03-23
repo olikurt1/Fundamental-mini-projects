ed93ce0813fb3999434e71d6fcb25dd329eb07e4
olikurt1 <olikurt1@outlook.com>
2026-03-23 13:11:04 +0000

    Covering while loop

---
+++ whileLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class whileLoop{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int iterations = 0;
+        int total = 0;
+        while (iterations != 10){
+            System.out.print("What is your number: ");
+            int number = input.nextInt();
+            total += number;
+            iterations ++;
+            
+        }
+        System.out.println(total);
+    }
+}
---
+++ whileLoop.md
@@ -0,0 +1,3 @@
+The while loop has the same functionality as a for loop, it allows for iteration. It is condition based however where the loop won't carry on for a fixed number of iterations but instead only once a condition is met. 
+
+It can do it until a certain value is just below or until a value is equal to the condition
