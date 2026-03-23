581c85d15d62f273e4251d4f7dd21de0d334e87c
olikurt1 <olikurt1@outlook.com>
2026-03-23 14:40:14 +0000

    Adding selection example and internals of selection

---
+++ selection.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class selection{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your number: ");
+        int userNumber = input.nextInt();
+        //if statement version
+        if(userNumber < 1){
+            System.out.println("Incorrect value range (tooLittle)");
+
+        }
+        else if(userNumber > 1000){
+            System.out.println("Incorrect value range (tooMuch)");
+        }
+        else if(userNumber  <=1000 && userNumber >= 1 ){
+            System.out.printf("%d is in the correct range.", userNumber);
+        }
+    }
+}
---
+++ selection.md
@@ -0,0 +1,6 @@
+Selection works through going over different conditions/branches and determining which condition is satisfied so that then the program can continue on that branch and achieve its goal
+
+If statements can be used to achieve this intuitively where if(certain condition is met) then this execution occurs etc. 
+
+If statements are more flexible because they allow for more complex conditions per branch while switch case requires the condtion to be a fixed value. 
+
