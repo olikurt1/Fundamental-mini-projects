f213ebc8f40a28cd2bbae13f3547a1d3818748a3
olikurt1 <145839522+olikurt1@users.noreply.github.com>
2026-04-01 13:45:42 +0100

    Writing from an array

--- programs/fileIO.java
+++ programs/fileIO.java
@@ -3,10 +3,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 public class fileIO{
     public static void main(String[] args)throws IOException{
+        String[] names = {"Ollie", "John", "Mike"};
         //file writing 
         BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
         writer.write("Writing to the file");
-        writer.write("\nHere is another line.");
+        writer.write("\nHere is another line.\n");
+        for(String name:names){
+            writer.write(name+"\n");
+        }
         writer.close();
     }
 }
